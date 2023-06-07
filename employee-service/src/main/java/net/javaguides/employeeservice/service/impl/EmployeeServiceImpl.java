ef17d2d1eeafee1508bf2ad5479677e6ecbece16
Rodrigo GrauU <rodrigoassis1997@gmail.com.br>
2023-06-07 10:29:12 -0300

    1439 && 140 && 141. Circuit Breaker Pattern implementation using Resilience4j
    Adicionado o Resilience4J. Foi utilizado o Web Client no lugar do Spring Cloud OpenFeign para os exemplos utilizando o Circuit Breaker.
    Também foi habilitado as informações no health check do Actuator referente ao Employee Service.

--- employee-service/src/main/java/net/javaguides/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/net/javaguides/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -1,14 +1,16 @@
 package net.javaguides.employeeservice.service.impl;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.AllArgsConstructor;
 import net.javaguides.employeeservice.dto.ApiResponseDto;
 import net.javaguides.employeeservice.dto.DepartmentDto;
 import net.javaguides.employeeservice.dto.EmployeeDto;
 import net.javaguides.employeeservice.entity.Employee;
 import net.javaguides.employeeservice.repository.EmployeeRepository;
-import net.javaguides.employeeservice.service.ApiClient;
+//import net.javaguides.employeeservice.service.ApiClient;
 import net.javaguides.employeeservice.service.EmployeeService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
@@ -17,8 +19,8 @@ public class EmployeeServiceImpl implements EmployeeService {
     private EmployeeRepository employeeRepository;
 
 //    private RestTemplate restTemplate;
-//    private WebClient webClient;
-    private ApiClient apiClient;
+    private WebClient webClient;
+//    private ApiClient apiClient;
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -41,6 +43,7 @@ public class EmployeeServiceImpl implements EmployeeService {
         return savedEmployeeDto;
     }
 
+    @CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
     @Override
     public ApiResponseDto getEmployeeById(Long employeeId) {
         Employee employee = employeeRepository.findById(employeeId).get();
@@ -50,15 +53,36 @@ public class EmployeeServiceImpl implements EmployeeService {
         DepartmentDto departmentDto = responseEntity.getBody();
         */
 
-        /*
          DepartmentDto departmentDto = webClient.get()
                 .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
                 .retrieve()
                 .bodyToMono(DepartmentDto.class)
                 .block();
-         */
 
-        DepartmentDto departmentDto = apiClient.getDepartment(employee.getDepartmentCode());
+//        DepartmentDto departmentDto = apiClient.getDepartment(employee.getDepartmentCode());
+
+        EmployeeDto employeeDto = new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail(),
+                employee.getDepartmentCode()
+        );
+
+        ApiResponseDto apiResponseDto = new ApiResponseDto();
+        apiResponseDto.setEmployeeDto(employeeDto);
+        apiResponseDto.setDepartmentDto(departmentDto);
+
+        return apiResponseDto;
+    }
+
+    public ApiResponseDto getDefaultDepartment(Long employeeId, Exception e) {
+        Employee employee = employeeRepository.findById(employeeId).get();
+
+        DepartmentDto departmentDto = new DepartmentDto();
+        departmentDto.setDepartmentName("R&G Department");
+        departmentDto.setDepartmentCode("RD001");
+        departmentDto.setDepartmentDescription("Research and Development Departmennt");
 
         EmployeeDto employeeDto = new EmployeeDto(
                 employee.getId(),
